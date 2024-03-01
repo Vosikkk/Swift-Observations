76c7ef3e12afde4ed2207f92af15dc292c19d004
Vosik <vosikvos@gmail.com>
2024-03-01 19:37:58 +0200

    finished

--- Sources/ObservationsClient/main.swift
+++ Sources/ObservationsClient/main.swift
@@ -54,13 +54,14 @@ class Suspect: Observable {
 }
 
 let suspect = Suspect(name: "Darth Vader", suspiciousness: 33)
+let suspect2 = Suspect(name: "Old Name", suspiciousness: 45)
 
 
 var ON_CHANGE_CLOSURE: (@Sendable () -> Void)?
 
 class ObservationRegistrar: @unchecked Sendable {
     
-    var registry: [AnyKeyPath: Set<Observation.ID>] = [:]
+    var lookups: [AnyKeyPath: Set<Observation.ID>] = [:]
     var observations: [Observation.ID: Observation] = [:]
     
     struct Observation: Identifiable {
@@ -69,34 +70,52 @@ class ObservationRegistrar: @unchecked Sendable {
         let closure: @Sendable () -> Void
     }
     
-    func registerOnChange(
-        _ keyPaths: Set<AnyKeyPath>,
-        _ onChange: @escaping @Sendable () -> Void
-    ) {
-        let observation = Observation(keyPaths: keyPaths, closure: onChange)
-        observations[observation.id] = observation
-        for keyPath in keyPaths {
-            registry[keyPath, default: []].insert(observation.id)
+    func cancel(_ observationId: Observation.ID) {
+        print("Should Cancel \(observationId)")
+        
+        if let observation = observations.removeValue(forKey: observationId) {
+            for keyPath in observation.keyPaths {
+                lookups[keyPath]?.remove(observation.id)
+                if (lookups[keyPath]?.isEmpty ?? false) {
+                    lookups.removeValue(forKey: keyPath)
+                }
+            }
         }
     }
     
+    func registerOnChange(_ keyPaths: Set<AnyKeyPath>, _ onChange: @escaping @Sendable () -> Void) -> Observation.ID {
+        let observation = Observation(keyPaths: keyPaths, closure: onChange)
+        observations[observation.id] = observation
+        for keyPath in keyPaths {
+            lookups[keyPath, default: []].insert(observation.id)
+        }
+        return observation.id
+    }
     
-    func access<Subject, Member>(
-        _ subject: Subject,
-        keyPath: KeyPath<Subject, Member>
-    ) where Subject : Observable {
+    
+    func access<Subject, Member>(_ subject: Subject, keyPath: KeyPath<Subject, Member>) where Subject : Observable {
         print("access \(keyPath)")
         GLOBAL_ACCESS_LIST?.trackAccess(self, keyPath: keyPath)
     }
     
     
-    func withMutation<Subject, Member, T>(
-        of subject: Subject,
-        keyPath: KeyPath<Subject, Member>,
-        _ mutation: () throws -> T
-    ) rethrows -> T where Subject : Observable {
+    func withMutation<Subject, Member, T>(of subject: Subject, keyPath: KeyPath<Subject, Member>, _ mutation: () throws -> T) rethrows -> T where Subject : Observable {
         print("mutation \(keyPath)")
         
+        if let observationIds = lookups.removeValue(forKey: keyPath) {
+            for observationId in observationIds {
+                if let observation = observations.removeValue(forKey: observationId) {
+                    observation.closure()
+                    
+                    for keyPath in observation.keyPaths {
+                        lookups[keyPath]?.remove(observation.id)
+                        if (lookups[keyPath]?.isEmpty ?? false) {
+                            lookups.removeValue(forKey: keyPath)
+                        }
+                    }
+                }
+            }
+        }
         return try mutation()
     }
 }
@@ -112,29 +131,50 @@ struct AccessList {
     
     var entries: [ObjectIdentifier: Entry] = [:]
     
+    
     mutating func trackAccess(_ registrar: ObservationRegistrar, keyPath: AnyKeyPath) {
         let id = ObjectIdentifier(registrar)
         entries[id, default: Entry(registrar: registrar)].keyPaths.insert(keyPath)
     }
     
+    
     func registerOnChange(_ onChange: @escaping @Sendable () -> Void) {
+        
+        
+        let observationIds: Box<[ObjectIdentifier: ObservationRegistrar.Observation.ID]> = Box(value: [:])
+        
+        let cancellingOnChange: @Sendable () -> Void = {
+            onChange()
+            
+            // cancel the observations on every registrar
+            
+            for (registrarId, observationId) in observationIds.value {
+                entries[registrarId]?.registrar.cancel(observationId)
+            }
+        }
+        
         for entry in entries {
             let registrar = entry.value.registrar
             let keyPaths = entry.value.keyPaths
-            registrar.registerOnChange(
-                keyPaths,
-                onChange
-            )
+            
+            let observationId = registrar.registerOnChange(keyPaths,cancellingOnChange)
+            
+            observationIds.value[entry.key] = observationId
         }
     }
 }
 
+class Box<A> {
+    
+    var value: A
+    
+    init(value: A) {
+        self.value = value
+    }
+}
 
 
-public func withObservationTracking<T>(
-    _ apply: () -> T,
-    onChange: @escaping @Sendable () -> Void
-) -> T {
+public func withObservationTracking<T>(_ apply: () -> T, onChange: @escaping @Sendable () -> Void) -> T {
     // 1. Set a global closure
     // 2. call apply()
     // 3. suspect.access(\.name)
@@ -150,10 +190,15 @@ public func withObservationTracking<T>(
 
 
 withObservationTracking {
-    print("I am observing \(suspect.name) \(suspect.suspiciousness)/")
+    print("I am observing \(suspect.name) \(suspect.suspiciousness)")
+    print("I am observing \(suspect2.name) \(suspect2.suspiciousness)")
 } onChange: {
   print("Name changed!")
 }
 
 suspect.name = "New Name"
-suspect.suspiciousness = 12
+suspect2.suspiciousness = 12
+
+
+suspect2.name = "New Name"
+suspect.suspiciousness = 10
